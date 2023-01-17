ef655d22ea2ef1a383bbaf93e21d3bebfedf8c63
47cum <109858210+47cum@users.noreply.github.com>
2023-01-17 14:34:10 +0900

    [D1] Title: 평균값 구하기, Time: 93 ms, Memory: 42,716 KB -BaekjoonHub

--- SWEA/D1/2071. 평균값 구하기/평균값 구하기.py
+++ SWEA/D1/2071. 평균값 구하기/평균값 구하기.py
@@ -1,9 +1,9 @@
 T = int(input())
 
 for test_case in range(1, T + 1):
-    sum = 0
+    summ = 0
     num = list(map(int,input().split()))
     for idx in num :
-        sum += idx
-    ave = round(sum / len(num))
+        summ += idx
+    ave = round(summ / len(num))
     print(f'#{test_case} {ave}')
