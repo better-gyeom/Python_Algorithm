a54144aa28520685694540dba731ab2aa92600dc
유은겸 <yueungyeom@yueungyeom-ui-MacBookAir.local>
2023-01-25 23:16:20 +0900

    상근날드 코드리뷰

--- 백준/Bronze/5543. 상근날드/상근날드.py
+++ 백준/Bronze/5543. 상근날드/상근날드.py
@@ -1,12 +1,19 @@
-prices = []
-total_price = []
 
-for n in range(5):
-    price = int(input()) # 버거의 값 3개와 음료 값 2개 입력 받기
-    prices.append(price) 
+burger_price = []
+drink_price = []
+set_price = []
 
-for burger in prices[:3]:
-    for drink in prices[3:]:
-        total_price.append((burger + drink) - 50) # 이중 for문을 통해 가능한 세트 가격을 만들고, 50원을 뺀 리스트 만들기
+#for문을 따로따로 선언하면 관리하기 수월하다.
+for _ in range(3):
+    price = int(input()) 
+    burger_price.append(price)
+
+for _ in range(2):
+    price = int(input())
+    drink_price.append(price)
+
+for burger in burger_price:
+    for drink in drink_price:
+        set_price.append((burger + drink) - 50) # 이중 for문을 통해 가능한 세트 가격을 만들고, 50원을 뺀 리스트 만들기
         
-print(min(total_price)) # 세트 가격 최솟값
+print(min(set_price)) # 세트 가격 최솟값
