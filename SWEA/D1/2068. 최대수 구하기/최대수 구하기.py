7b2fffbf01477e03bbdc83a7425326ef70ff604a
47cum <109858210+47cum@users.noreply.github.com>
2023-01-26 14:37:18 +0900

    [D1] Title: 최대수 구하기, Time: 90 ms, Memory: 43,264 KB -BaekjoonHub

--- SWEA/D1/2068. 최대수 구하기/최대수 구하기.py
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.py
@@ -1,12 +1,4 @@
 T = int(input())
-
-for test_case in range(1,T+1) :
-    num = list(map(int,input().split()))
-    max = num[0]
-    for i in range(len(num)) :
-        if num[i] > max :
-            max = num[i]
-    print(f'#{test_case} {max}')
-    max = 0
-    
-        
+for test_case in range(1,T+1):
+    nums = list(map(int,input().split()))
+    print(f'#{test_case} {max(nums)}')
