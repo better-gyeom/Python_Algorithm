fc73263e246a06d1c9e66a1bb684e19e635c61ad
better-gyeom <125348113+better-gyeom@users.noreply.github.com>
2023-02-22 16:08:50 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -0,0 +1,7 @@
+arr = set([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30])
+home = set()
+for _ in range(28):
+    n = int(input())
+    home.add(n)
+for i in sorted(arr - home):
+    print(i)
