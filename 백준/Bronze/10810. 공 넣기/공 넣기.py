515455d11d726f52b77828afc8c05b0e3970209d
better-gyeom <125348113+better-gyeom@users.noreply.github.com>
2023-02-22 15:52:44 +0900

    [Bronze II] Title: 공 넣기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,7 @@
+N, M = map(int, input().split())
+arr = [0] * (N + 1)
+for m in range(M):
+    i, j, k = map(int, input().split())
+    for n in range(i, j + 1):
+        arr[n] = k
+print(*arr[1:])
